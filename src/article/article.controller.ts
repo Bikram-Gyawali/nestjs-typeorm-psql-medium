b39fb6eb472a1d3f15fce81f358ad8b0c25b36db
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-09-06 23:26:15 +0545

    article section almost complete for now

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,65 @@
+import { ArticleResponseInterface } from './types/articleResponse.interface';
+import { UserEntity } from 'src/user/user.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { AuthGuard } from './../user/guards/auth.guard';
 import { ArticleService } from './article.service';
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from 'src/user/decorators/user.decorator';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
-  async create() {
-    return this.articleService.createArticle;
+  @UseGuards(AuthGuard)
+  async create(
+    @User('id') currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(':slug')
+  async getSingleArticle(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.findBySlug(slug);
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Delete(':slug')
+  @UseGuards(AuthGuard)
+  async deleteArticle(
+    @User('id') currentUserId: number,
+    @Param('slug') slug: string,
+  ) {
+    return await this.articleService.deleteArticle(slug, currentUserId);
+  }
+
+  @Put(':slug')
+  @UseGuards(AuthGuard)
+  async updateArticle(
+    @User('id') currentUserId: number,
+    @Param('slug') slug: string,
+    updadteArticleDto: CreateArticleDto,
+  ) {
+    const article = await this.articleService.updteArticle(
+      slug,
+      updateArticle,
+      currentUserId,
+    );
+    return await this.articleService.buildArticleResponse(article);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,73 @@
-import { Injectable } from '@nestjs/common';
-
+import { UserEntity } from 'src/user/user.entity';
+import { ArticleEntity } from './article.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { ArticleResponseInterface } from './types/articleResponse.interface';
+import slugify from 'slugify';
 @Injectable()
 export class ArticleService {
-  async createArticle() {
-    return 'createArticle from service';
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+    article.slug = this.getSlug(createArticleDto.title);
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
+  }
+
+  async findBySlug(slug: string): Promise<ArticleEntity> {
+    return await this.articleRepository.findOne({ where: { slug } });
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
+  }
+
+  async deleteArticle(slug, currentUserId: number): Promise<DeleteResult> {
+    const article = this.findBySlug(slug);
+    if (!article)
+      throw new HttpException('Article dos not exist ', HttpStatus.NOT_FOUND);
+
+    if (article.author.id !== currentUserId) {
+      throw new HttpException('You are not an author', HttpStatus.FORBIDDEN);
+    }
+    return await this.articleRepository.delete({ slug });
+  }
+
+  async updteArticle(
+    slug: string,
+    currentUserId: number,
+    updateArticle: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = await this.findBySlug(slug);
+    if (!article)
+      throw new HttpException('Article not exist', HttpStatus.NOT_FOUND);
+    if (this.articleRepository.author.id !== currentUserId)
+      throw new HttpException('You are not an author', HttpStatus.FORBIDDEN);
+
+    Object.assign(article, updateArticle);
+
+    return await this.articleRepository.save(article);
   }
 }
