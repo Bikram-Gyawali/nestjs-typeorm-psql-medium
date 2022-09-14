142964c5c263d2b7d96a4d975b70ceea92c05f7e
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-09-14 23:25:40 +0545

    user profile

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,10 @@
+import { FollowEntity } from './follow.entity';
 import { UserEntity } from './../user/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, FollowEntity])],
   controllers: [],
   providers: [],
 })
