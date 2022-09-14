d9eaac228b00ac5c93947dd68887d55de88ac6af
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-09-14 19:30:40 +0545

    follow user service

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -32,6 +32,36 @@ export class ProfileService {
     return { ...user, following: Boolean(follow) };
   }
 
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'Follower and FOllowing can not be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const follow = await this.followRepository.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+    return { ...user, following: true };
+  }
+
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
     delete profile.email;
     return { profile };
